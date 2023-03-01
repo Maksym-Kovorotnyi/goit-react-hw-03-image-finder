0f55aa6ba5e3bb89e1b2276831d679709cc5f6a7
Maksym-Kovorotnyi <Kovorotny1@gmail.com>
2023-03-01 10:21:14 +0200

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,9 +1,6 @@
 import css from './ImageGallery.module.css'
 import { Item } from 'components/ImageGalleryItem/ImageGalleryItem'
 
-
-
-
 export const Gallery =({images, onClick, Loader}) =>{
         return (<>
             <ul className={css.imageGallery}>
